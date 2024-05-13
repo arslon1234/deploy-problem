dd5f7346bb3f9480419ee51ad6c863040a6ad151
Arslonbek <roziboyevarslon0110@gmail.com>
2024-05-13 10:27:01 +0500

    login

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -1,20 +1,28 @@
-import { useEffect } from "react"
-import axios from "axios"
+import axios from "axios";
 const First = () => {
-  useEffect(()=>{
-    axios.get("https://jsonplaceholder.typicode.com/users").then(res=>{
-      console.log(res)
-    })
-  },[])
+ const login =()=>{
+  axios
+      .post("http://store.go-clothes.uz:5555/v1/login", {
+        email: "xasannosirov094@gmail.com",
+        password: "Sehtols@01",
+      })
+      .then((res) => {
+        console.log(res);
+      });
+ }
   return (
     <div>
       <h1>First components</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, soluta? Obcaecati ullam cupiditate amet ad dolores illum, minima perferendis sed saepe. Cupiditate optio blanditiis quaerat eius commodi dolore, corporis autem.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, soluta?
+        Obcaecati ullam cupiditate amet ad dolores illum, minima perferendis sed
+        saepe. Cupiditate optio blanditiis quaerat eius commodi dolore, corporis
+        autem.
       </p>
       <h3>http sorov</h3>
+      <button onClick={login}>login</button>
     </div>
-  )
-}
+  );
+};
 
-export default First
+export default First;
